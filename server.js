0083a2a7aea259ac4db5fb47d1d0acba421641e3
AbdulBaseer7896 <abdulbasirqazi7896@gmail.com>
2025-04-11 00:27:31 +0500

    just change the  frontend url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 // Connect to MongoDB
 connectDB();
 const corsOptions = {
-    origin: process.env.FRONTEND_ORIGIN || "https://enchanting-lolly-86d5a6.netlify.app/",
+    origin: process.env.FRONTEND_ORIGIN || "https://bitnextdailer.netlify.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
